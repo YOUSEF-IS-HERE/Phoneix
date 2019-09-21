03931f60b2a069f154151b6377f7aa6d791eada3
YOUSEF-IS-HERE <40815405+YOUSEF-IS-HERE@users.noreply.github.com>
2019-09-21 13:13:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,6 +88,18 @@ client.on('message', async (message) => {
     });
   }
 });
+//tag
+const figlet = require('figlet');
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'tag')) {
+    let args = message.content.split(" ").slice(1);
+if(!args[0]) return message.reply('مرجو كتابة نص الدي تريد');  
+
+    figlet(args.join(" "), (err, data) => {
+              message.channel.send("```" + data + "```")
+           })
+}
+});
 
 
 
