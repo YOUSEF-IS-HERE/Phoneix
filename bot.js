b7bf9f83c582740b5042cba113c4cf5be25597c5
YOUSEF-IS-HERE <40815405+YOUSEF-IS-HERE@users.noreply.github.com>
2020-10-01 13:23:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -204,5 +204,20 @@ client.on("message", function(message) {
        
    }
 });
+//members 
+client.on('message', message => {
+    if (message.author.bot) return;
+    if(message.content == '+member') {
+    const embed = new Discord.RichEmbed()
+    .addField(`حالة الأعضاء🔋`,'-',   true)
+.addField(`💚 اونلاين:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}`,'-',   true)
+.addField(`❤ مشغول:     ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`,'-',   true)
+.addField(`💛 خامل:      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}`,'-',   true)   
+.addField(`🖤 اوفلاين:   ${message.guild.members.filter(m=>m.presence.status == 'offline').size}`,'-',  true) 
+.addField(`💙   الكل:  ${message.guild.memberCount}`,'-',   true)     
+    .setFooter(`SN bot `,'https://cdn.discordapp.com/attachments/448412582055903232/463505714929991691/f.jpg')
+         message.channel.send({embed});
 
+    }
+  });
 client.login(process.env.BOT_TOKEN);
