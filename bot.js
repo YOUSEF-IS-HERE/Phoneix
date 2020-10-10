93bc6f5099358fb6a729d6ed21ee01e0e9c4c60e
YOUSEF-IS-HERE <40815405+YOUSEF-IS-HERE@users.noreply.github.com>
2020-10-10 20:18:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,44 +29,27 @@ client.user.setGame(`Phoneix's Bot`,"http://twitch.tv/S-F")
 });
 
 //clear
-client.on('message', message => {
-   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'clear')) {
-if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Do not have permission** `ADMINISTRATOR`' );
-  let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let request = `Requested By ${message.author.username}`;
-message.channel.send(`**هل انت متأكد من أنك تريد محو الشات؟ **`).then(msg => {
-msg.react('✅')
-.then(() => msg.react('❌'))
-.then(() =>msg.react('✅'))
-
-let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-
-let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-reaction1.on("collect", r => {
-message.channel.send(`Chat will delete`).then(m => m.delete(5000));
-var msg;
+client.on("message", message => {
+    var prefix = "+"; // غير هنا حط البرفكس
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(':warning: | ليس لديك صلاحيات');
+        var msg;
         msg = parseInt();
 
       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-  message.channel.sendMessage("", {embed: {
-        title: "`` Chat Deleted ``",
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
         color: 0x06DF00,
+        description: "تم مسح 100 رسالة بنجاح",
         footer: {
-
+          text: "Boom" // غير هنا حط اسم البوت
         }
       }}).then(msg => {msg.delete(3000)});
+                          }
 
-})
-reaction2.on("collect", r => {
-message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
-msg.delete();
-})
-})
-}
+
 });
 //server
 client.on('message', function(msg) {
@@ -355,25 +338,7 @@ client.on('message', message => {
     })
     }
     });
-client.on("message", message => {
-    var prefix = "+"; // غير هنا حط البرفكس
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(':warning: | ليس لديك صلاحيات');
-        var msg;
-        msg = parseInt();
 
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم",
-        color: 0x06DF00,
-        description: "تم مسح 100 رسالة بنجاح",
-        footer: {
-          text: "Boom" // غير هنا حط اسم البوت
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
 //cat
 client.on('message', message => {
     if (message.content === 'حمودي') {
